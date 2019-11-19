05a5df670a30b3d326cdbe8126aec0775f956c34
DavidBejarano <dave-2709@hotmail.com>
2019-11-19 11:35:05 -0500

    Ajuste Controlador Agenda

--- src/java/controlador/AgendaControlador.java
+++ src/java/controlador/AgendaControlador.java
@@ -62,12 +62,8 @@ public class AgendaControlador extends HttpServlet {
                 break;
 
         }
-<<<<<<< HEAD
-
     }
-=======
-    
->>>>>>> desarrollo
+
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
